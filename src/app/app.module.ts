b8deab1b1be83d0e53b3a71cb42813c2fd3557fc
Roger Becerra <rogelio.becerra001@gmail.com>
2021-05-10 14:23:00 -0500

    bugs fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { SignupComponent } from "./signup/signup.component";
+import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { CategoriesComponent } from './categories/categories.component';
 
